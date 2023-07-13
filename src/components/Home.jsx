aa15d490002bbe8588ca817d2bbbddf3e9999945
ANKUSH ARORA <127427325+Ankush-Arora-0@users.noreply.github.com>
2023-07-13 21:49:44 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ export const Home = (props) => {
 
     const HomeData = async () => {
         try {
-            const res = await fetch('http://localhost:5000/home', {
+            const res = await fetch('https://ecommerce-bac.onrender.com/home', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
